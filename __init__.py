4bee254f7e244f398afff8d909e491603c06d873
WJ714 <zhangwj714@163.com>
2022-04-08 11:29:18 +0200

    add __init__

---
+++ __init__.py
@@ -0,0 +1,8 @@
+__title__ = 'HRHC'
+__version__ = '0.0.0'
+__author__ = 'Weijie_Zhang & Jake Nelson & Martin Jung'
+__author_email__ = 'wzhang@bgc-jena.mpg.de'
+__license__ = 'GNU GPLv3'
+__copyright__ = 'Copyright 2022 by MPI-BGC-GDM'
+
+from HRHC import xgb4caglar
